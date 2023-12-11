754d4cdc07a53ad279f0ccab44e431995334d5c1
Bilal Oulahal <Bilal.OULAHAL@etu.uca.fr>
2023-12-11 09:40:15 +0100

    ModifIndex

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const { engine } = require('express-handlebars');
+const session = require('express-session');
 
 const app = express();
 const port = 3010;
 
 app.use(express.static('static'));
+
+//Body-parser pour récupérer les POST
+app.use(express.urlencoded({ extended: true }));
+
+// Configuration de la session
+app.use(session({
+    secret: 'SECRET',
+    resave: false,
+    saveUninitialized: true
+}));
 
 const DashboardRouter = express.Router();
 app.use('/dashboard', DashboardRouter);
