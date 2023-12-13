a85c5c3fd01c56e17cdbd154910673597814b696
Bilal Oulahal <Bilal.OULAHAL@etu.uca.fr>
2023-12-13 07:04:35 +0100

    Modif routes

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ app.use(express.urlencoded({ extended: true }));
 
 // Configuration de la session
 app.use(session({
-    secret: 'SECRET',
-    resave: false,
-    saveUninitialized: true
+  secret: 'SECRET',
+  resave: false,
+  saveUninitialized: true
 }));
 
 const DashboardRouter = express.Router();
@@ -26,7 +26,7 @@ app.set('view engine', 'handlebars');
 app.set('views', './views');
 
 app.get('/', (req, res) => {
-    res.redirect('/dashboard');
+  res.redirect('/dashboard');
 });
 
 DashboardRouter.get('/', (req, res) => {
@@ -42,29 +42,25 @@ DashboardRouter.get('/', (req, res) => {
 
   // Données pour les rappels
   for (let i = 1; i <= 10; i++) {
-    data.rappels.push({ titre: `titre_rappel ${i}`, date: `date_rappel ${i}` , description: `description ${i}` });
+    data.rappels.push({ titre: `titre_rappel ${i}`, date: `date_rappel ${i}`, description: `description ${i}` });
   }
 
   res.render('home', data);
 });
 
-DashboardRouter.get('/inscription', (req, res) => {
+app.get('/inscription', (req, res) => {
   res.render('Inscription');
 });
 
-DashboardRouter.get('/contact', (req, res) => {
-  res.render('Contact');
-});
-
-DashboardRouter.get('/connexion', (req, res) => {
+app.get('/connexion', (req, res) => {
   res.render('Connexion');
 });
 
 // Gestion des erreurs 404 pour les routes non définies
 app.use((req, res, next) => {
-    res.status(404).send('erreur 404 Page not found');
+  res.status(404).send('erreur 404 Page not found');
 });
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
