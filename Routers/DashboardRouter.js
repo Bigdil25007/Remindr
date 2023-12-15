a85f242a2934564f100fc3659d2c6781e3ee7b7c
Bilal Oulahal <Bilal.OULAHAL@etu.uca.fr>
2023-12-15 08:22:51 +0100

    dashboardroute

--- Routers/DashboardRouter.js
+++ Routers/DashboardRouter.js
@@ -2,27 +2,28 @@ const express = require('express');
 const routeur = express.Router();
 
 routeur.get('/dashboard', (req, res) => {
-  if (req.session.user) {
-    const data = {
-      groups: [],
-      rappels: []
-    };
-
-    // Données pour les groupes
-    for (let i = 1; i <= 10; i++) {
-      data.groups.push({ nom: `nom_groupe ${i}`, GroupeID: i });
-    }
-
-    // Données pour les rappels
-    for (let i = 1; i <= 10; i++) {
-      data.rappels.push({ titre: `titre_rappel ${i}`, date: `date_rappel ${i}`, description: `description ${i}` });
-    }
-
-    res.render('home', data);
+  if (!req.session.user) {
+    res.render('blocked');
+    return;
   }
 
-  else
-    res.render('blocked');
+  const data = {
+    groups: [],
+    rappels: []
+  };
+
+  // Données pour les groupes
+  for (let i = 1; i <= 10; i++) {
+    data.groups.push({ nom: `nom_groupe ${i}`, GroupeID: i });
+  }
+
+  // Données pour les rappels
+  for (let i = 1; i <= 10; i++) {
+    data.rappels.push({ titre: `titre_rappel ${i}`, date: `date_rappel ${i}`, description: `description ${i}` });
+  }
+
+  res.render('home', data);
+
 });
 
 
