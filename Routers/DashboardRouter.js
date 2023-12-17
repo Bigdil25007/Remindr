67aea305fb750081d7c7a6be8be0f7474445c064
Bilal Oulahal <Bilal.OULAHAL@etu.uca.fr>
2023-12-17 12:48:04 +0100

    gestion rappels depasssé

--- Routers/DashboardRouter.js
+++ Routers/DashboardRouter.js
@@ -23,6 +23,38 @@ async function findRappels(groupId) {
   });
 }
 
+function FormatterTab(tableau) {
+  /*
+  On récupère le tableau de rappels et on passe tous les éléments à la même profondeur
+  On a donc un tableau à 1 dimension au lieu de 2
+  Ensuite on trie le tableau en fonction des dates dans l'ordre décroissant
+  */
+  let tabFormat = tableau.flat().sort(function (a, b) {
+    // Convertir les dates en objets Date
+    let dateA = new Date(a.dateFin);
+    let dateB = new Date(b.dateFin);
+
+    return dateB - dateA;
+  });
+
+  /* 
+  On va séparer le tableau en 2 sous tableaux pour séparer les rappels dépassés de ceux à faire
+  */
+  let rappelsAfaire = [];
+  let rappelsDepasse = [];
+  const currentTimestamp = new Date();
+
+  tabFormat.forEach(rappel => {
+    if (rappel.dateFin < currentTimestamp) {
+      rappelsDepasse.push(rappel);
+    } else {
+      rappelsAfaire.push(rappel);
+    }
+  });
+
+  return { rappelsAfaire, rappelsDepasse };
+};
+
 
 routeur.get('/dashboard', async (req, res) => {
   if (!req.session.user) {
@@ -32,7 +64,8 @@ routeur.get('/dashboard', async (req, res) => {
 
   let data = {
     groups: [],
-    rappels: []
+    rappelsAfaire: [],
+    rappelsDepasse: []
   };
 
   //Récupération des groupes
@@ -42,14 +75,16 @@ routeur.get('/dashboard', async (req, res) => {
   let rappels = [];
 
   for (group in data.groups) {
-    rappels = await findRappels(group.IDGroup);
-    data.rappels.push(rappels);
+    resultat = await findRappels(group.IDGroup);
+    rappels.push(resultat);
   }
 
-  data.rappels = data.rappels.flat();
+  const { rappelsAfaire, rappelsDepasse } = FormatterTab(rappels);
+
+  data.rappelsAfaire = rappelsAfaire;
+  data.rappelsDepasse = rappelsDepasse;
 
   res.render('home', data);
-
 });
 
 
